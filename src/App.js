811afa27bb0d2854e50ac0c65293b5c062976d33
Elena <freshjs.ftm@gmail.com>
2021-11-30 11:18:12 +0200

    Prepare for YUP & FORMIC

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import './App.css';
 import React, {Component} from 'react';
-import {UserContext, ThemeContext} from './context';
-import Header from './components/Header';
-import CONSTANTS from './constants';
-import TreeWithTheme from './components/Tree';
-import HeaderWithTheme from './components/Header';
-const {THEMES} = CONSTANTS;
+import SignInForm from './components/SignInForm';
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme:THEMES.LIGHT,
-      user:{
-        id:1,
-        fname:'Elon',
-        sname:'Musk',
-        imgSrc:'https://i.insider.com/6171279833f4b300189ad407?width=700'
-      }
-    }
-  }
-  
-  setTheme = (theme) => {this.setState({theme})}
 
-  render(){
-    const {user, theme} = this.state;
-    console.log(UserContext);
-    return <ThemeContext.Provider value={[theme,this.setTheme]}>
-      <UserContext.Provider value={user}>
-        <HeaderWithTheme />
-        <TreeWithTheme/>
-        </UserContext.Provider>
-      </ThemeContext.Provider>;
-  }
-  
+function App(){
+  return(
+    <>
+    <SignInForm />
+    </>
+  );
 }
 
 export default App;
