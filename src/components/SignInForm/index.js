0e295b1ef3442c782966ec782967868c0a473d1d
Elena <freshjs.ftm@gmail.com>
2021-12-01 10:18:47 +0200

    Add formik

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
 import cx from 'classnames';
 import styles from './SignInForm.module.scss';
+import {SIGN_IN_SCHEMA} from '../../utils/schemasValidation';
 
-class SignInForm extends Component{
-  render(){
-    return null;
+const initialValues = {
+  email:'',
+  password:''
+}
+
+const SignInForm = (props) => {
+  const onSubmit = (values, formikBag) => {
+    console.log('values:', values)
+    //console.log('formikBag:', formikBag)
   }
+  return (
+    <Formik initialValues={initialValues} 
+    onSubmit={onSubmit}
+    validationSchema={SIGN_IN_SCHEMA}>{
+        (formikProps) =>{
+          //console.log(formikProps)
+          return(
+            <Form>
+                <Field name='email'/>
+                <Field name='password'/>
+                <button type="submit">Sign in</button>
+            </Form>
+          )
+        }
+    }</Formik>
+  );
 }
 
 export default SignInForm;
--- src/utils/schemasValidation.js
+++ src/utils/schemasValidation.js
@@ -1,9 +1,20 @@
 import * as Yup from 'yup';
 
+export const NAME_SCHEMA = Yup.string().matches(/^[A-Z][a-z]{1,16}$/,'Must be latin letter').required();
+
+export const EMAIL_SCHEMA = Yup.string().email('Enter correct email').required();
+
+export const PASSWORD_SCHEMA = Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$/,'Enter correct password').required();
+
 export const SIGN_UP_SCHEMA = Yup.object({
-  fname: Yup.string().matches(/^[A-Z][a-z]{1,16}$/,'Must be latin letter 1').required(), 
-  sname:Yup.string().matches(/^[A-Z][a-z]{1,26}$/,'Must be latin letter 2').required(),
-  email:Yup.string().email('Enter correct email').required(),
-  password:Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$/,'Enter correct password').required(),
+  fname: NAME_SCHEMA, 
+  sname: NAME_SCHEMA,
+  email: EMAIL_SCHEMA,
+  password:PASSWORD_SCHEMA,
   age: Yup.number().min(14).max(99),
+})
+
+export const SIGN_IN_SCHEMA = Yup.object({
+  email:EMAIL_SCHEMA,
+  password:PASSWORD_SCHEMA,
 })
