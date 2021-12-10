0a072429a7651cf0d51b7103171c44daea46331c
Elena <freshjs.ftm@gmail.com>
2021-12-10 10:02:21 +0200

    Add @mui

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import cx from 'classnames';
+import {NavLink} from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
+import styles from './NavMenu.module.scss';
 
 const NavMenu = () => {
+  const classNames = cx(styles.container);
   return (
-    <div>
-      
-    </div>
+    <nav className={classNames}>
+      <CloseIcon />
+      <ul className={styles.list}>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/signup'>Sign Up</NavLink>
+        </li>
+        <li>
+          <NavLink to='/chat'>Chat</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
