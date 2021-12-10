cbe126e6c17b5533331d61925ea0c31fa60722cb
Elena <freshjs.ftm@gmail.com>
2021-12-10 10:22:36 +0200

    Add AppContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import HomePage from './pages/HomePage';
-import {UserContext, ThemeContext} from './context';
+import {UserContext, ThemeContext, AppContext} from './context';
 import CONSTANTS from './constants';
 import SignUpForm from './components/forms/SignUpForm';
 import Chat from './components/Chat';
 import NavMenu from './components/NavMenu';
-const {THEMES} = CONSTANTS;
+import reducer from './appReducer';
+const {THEMES, ACTIONS} = CONSTANTS;
 
 function App(){
   const [user, setUser] = useState({
@@ -15,10 +17,12 @@ function App(){
     name:'Elon Musk'
   });
   const themeArrState = useState(THEMES.LIGHT);
+  const [state, dispatch] = useReducer(reducer, {isMenuOpen:false});
   return(
     <ThemeContext.Provider value={themeArrState}>
     <UserContext.Provider value={user}>
     <BrowserRouter>
+      <MenuOpenIcon />
       <NavMenu />
       <Routes>
         <Route path='/' element={<HomePage />} />
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,10 @@ const CONSTANTS = {
   THEMES:{
     DARK:'DARK',
     LIGHT:'LIGHT'
+  },
+  ACTIONS:{
+    MENU_OPEN:'MENU_OPEN',
+    MENU_CLOSE:'MENU_CLOSE'
   }
 }
 export default CONSTANTS;
