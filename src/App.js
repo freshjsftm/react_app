0625e8b6bbf45d4e558233d2bf64e6f9399a5568
Elena <freshjs.ftm@gmail.com>
2021-12-10 11:45:25 +0200

    Refactor ref

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState, useReducer} from 'react';
+import React, {useState, useReducer, useRef, useEffect} from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import HomePage from './pages/HomePage';
@@ -20,13 +20,27 @@ function App(){
   const [state, dispatch] = useReducer(reducer, {isMenuOpen:false});
   const openMenu = () => dispatch({type: ACTIONS.MENU_OPEN});
   const closeMenu = () => dispatch({type: ACTIONS.MENU_CLOSE});
+
+
+  const navRef = React.createRef();
+  useEffect(()=>{
+    const handlerClick = ({target})=>{
+      console.log(navRef)
+      if(state.isMenuOpen && !navRef.current.contains(target)){
+        closeMenu();
+      }
+    }
+    window.addEventListener('click', handlerClick)
+    return ()=> window.removeEventListener('click', handlerClick);
+  }, [state.isMenuOpen]);
+
   return(
     <AppContext.Provider value={{state, closeMenu}}>
     <ThemeContext.Provider value={themeArrState}>
     <UserContext.Provider value={user}>
     <BrowserRouter>
       <MenuOpenIcon onClick={openMenu}/>
-      <NavMenu />
+      <NavMenu navRef={navRef}/>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUpForm />} />
--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,17 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef, useEffect} from 'react';
 import cx from 'classnames';
 import {NavLink} from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
 import styles from './NavMenu.module.scss';
 import { AppContext } from '../../context';
 
-const NavMenu = () => {
+const NavMenu = (props) => {
   const {state:{isMenuOpen}, closeMenu} = useContext(AppContext);
   const classNames = cx(styles.container, {
     [styles.open]:isMenuOpen
   });
+  
+
+
   return (
-    <nav className={classNames}>
+    <nav className={classNames} ref={props.navRef}>
       <CloseIcon className={styles.closeBtn} onClick={closeMenu}/>
       <ul className={styles.list}>
         <li>
